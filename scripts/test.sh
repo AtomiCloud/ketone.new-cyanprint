2806a0471d88e0a0ea76990881d46c17e0e8985b
adelphi-liong <adelphi@atomi.cloud>
2026-03-24 23:31:36 -0700

    fix: reduce parallelism

--- scripts/test.sh
+++ scripts/test.sh
@@ -2,6 +2,5 @@
 set -euo pipefail
 GITHUB_REPO_REF="$(echo "${GITHUB_REPO_REF}" | tr '[:upper:]' '[:lower:]')"
 export GITHUB_REPO_REF
-PARALLELISM=$(($(nproc --all) * 4 > 16 ? 16 : $(nproc --all) * 4))
 cyanprint --version
-cyanprint test template . --parallel "$PARALLELISM"
+cyanprint test template . --parallel 4
